71a7c2beab1e77619680fda8afd7798a40b5a29f
andluis35 <and.luis35@gmail.com>
2025-09-16 22:01:40 -0300

    Add cors middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
-import express from 'express';
 import axios from 'axios';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("API rodando!")
+})
 
 app.get('/news', async (req, res) => {
     const category = req.query.category || 'top';
